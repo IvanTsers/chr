3916dd3669e521e563d7bed07c1c0e1f315017fb
IvanTsers <ivantsers@gmail.com>
2024-04-11 16:23:30 +0200

    first compilable version

---
+++ ancs.go
@@ -0,0 +1,130 @@
+package ancs
+
+import (
+	"github.com/evolbioinf/esa"
+	"github.com/evolbioinf/sus"
+	"github.com/ivantsers/fasta"
+	"sort"
+)
+
+type Seg struct {
+	s int
+	l int
+}
+
+func (seg *Seg) End() int {
+	return seg.s + seg.l - 1
+}
+func NewSeg() Seg {
+	return Seg{s: 0, l: 0}
+}
+func SortByStart(s []*Seg) []*Seg {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i].s < s[j].s
+	})
+	return s
+}
+func MinAncLen(l int, g float64, t float64) int {
+	x := 1
+	cq := 0.0
+	for cq < t {
+		x++
+		cq = cq + sus.Prob(l, g, x)
+	}
+	return x
+}
+func FindHomologies(
+	query *fasta.Sequence,
+	e *esa.Esa,
+	subjectLen int,
+	a int) []*Seg {
+	var qc, qp int
+	var mc, mp *esa.Minterval
+	//seg := NewSeg()
+	n := make(map[int]bool)
+	var seg Seg
+	var h []*Seg
+	areEquidist, rightAnchorFound := false, false
+	queryLen := query.Length()
+	for qc < queryLen {
+		queryPrefix := query.Data()[qc:queryLen]
+		mc = e.MatchPref(queryPrefix)
+		if mc.L > a && isUnique(mc) {
+			areEquidist = qc-qp == e.Sa[mc.I]-e.Sa[mp.I]
+			if qc > qp && areEquidist {
+				seg.l = seg.l + qc - e.Sa[mc.J] + mc.L
+				for pos := e.Sa[mp.J] + 1; pos < e.Sa[mc.I]; pos++ {
+					n[pos] = true
+				}
+				rightAnchorFound = true
+			} else {
+				if rightAnchorFound {
+					if seg.s > subjectLen {
+						seg.s = seg.s - subjectLen - 1
+					}
+					h = append(h, &seg)
+				}
+				seg.s = e.Sa[mc.I]
+				seg.l = mc.L
+				rightAnchorFound = false
+			}
+			qp = qc
+			mp = mc
+		}
+		qc = qc + mc.L + 1
+	}
+	SortByStart(h)
+	predecessor := make(map[int]int)
+	score := make(map[int]int)
+	visited := make(map[int]bool)
+	for i := -1; i < len(h); i++ {
+		predecessor[i] = 0
+		score[i] = 0
+		visited[i] = false
+	}
+	score[0] = h[0].s
+	maxScore, maxIndex := -1, -1
+	for i := 1; i < len(h); i++ {
+		maxScore = -1
+		maxIndex = -1
+		for k := 0; k < i-1; k++ {
+			if h[k].End() <= h[k].s {
+				if score[k] > maxScore {
+					maxScore = score[k]
+					maxIndex = k
+				}
+			}
+		}
+		predecessor[i] = maxIndex
+		score[i] = score[maxIndex] + h[i].l
+	}
+	s := argmaxMapInt(score)
+	for s > 0 {
+		visited[s] = true
+		s = predecessor[s]
+	}
+	var hred []*Seg
+	for i := 0; i < len(h); i++ {
+		if visited[i] {
+			hred = append(hred, h[i])
+		}
+	}
+	return hred
+}
+func isUnique(m *esa.Minterval) bool {
+	if m.J == m.I {
+		return true
+	} else {
+		return false
+	}
+}
+func argmaxMapInt(m map[int]int) int {
+	var maxKey, maxValue int
+	for key, value := range m {
+		if value > maxValue {
+			maxValue = value
+			maxKey = key
+		}
+	}
+	return maxKey
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module ancs
+
+go 1.21.5
+
+require (
+	github.com/evolbioinf/esa v0.0.0-20240208112648-445905ef2b6d
+	github.com/evolbioinf/sus v0.0.0-20230320163303-b6d16dd4ec1f
+	github.com/ivantsers/fasta v0.0.0-20240411115341-cc7b66bf9fed
+)
